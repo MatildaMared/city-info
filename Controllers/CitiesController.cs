d7b4cb687be404739f4610d64c45be38fab8b4a8
Matilda Mared <matildamared@live.se>
2022-04-11 09:47:21 +0200

    refactor controller to use ActionResult instead of JsonResult

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -1,3 +1,4 @@
+using CityInfo.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.Controllers;
@@ -7,14 +8,21 @@ namespace CityInfo.Controllers;
 public class CitiesController : ControllerBase
 {
     [HttpGet]
-    public JsonResult GetCities()
+    public ActionResult<IEnumerable<CityDto>> GetCities()
     {
-        return new JsonResult(CitiesDataStore.Current.Cities);
+        return Ok(CitiesDataStore.Current.Cities);
     }
 
     [HttpGet("{id}")]
-    public JsonResult GetCity(int id)
+    public ActionResult<CityDto> GetCity(int id)
     {
-        return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+        var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+
+        if (cityToReturn == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(cityToReturn);
     }
 }
