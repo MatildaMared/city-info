b9f3f6e7f0d920a8bf5d33946eef5cd632507308
Matilda Mared <matildamared@live.se>
2022-04-12 18:05:19 +0200

    implement logger

--- Controllers/PointsOfInterestController.cs
+++ Controllers/PointsOfInterestController.cs
@@ -8,6 +8,13 @@ namespace CityInfo.Controllers;
 [ApiController]
 public class PointsOfInterestController : ControllerBase
 {
+    private readonly ILogger<PointsOfInterestController> _logger;
+
+    public PointsOfInterestController(ILogger<PointsOfInterestController> logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+    
     [HttpGet]
     public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
     {
@@ -15,6 +22,7 @@ public class PointsOfInterestController : ControllerBase
 
         if (city == null)
         {
+            _logger.LogInformation("City with id {CityId} wasn't found when accessing points of interests", cityId);
             return NotFound();
         }
 
