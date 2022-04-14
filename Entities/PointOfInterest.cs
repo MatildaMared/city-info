50fa36248e7cd18800c34fd0cac4903f11184311
Matilda Mared <matildamared@live.se>
2022-04-14 14:07:20 +0200

    add property to point of interest

--- Entities/PointOfInterest.cs
+++ Entities/PointOfInterest.cs
@@ -13,6 +13,10 @@ public class PointOfInterest
     [MaxLength(50)]
     public string Name { get; set; }
     
+    [Required]
+    [MaxLength(200)]
+    public string Description { get; set; }
+    
     [ForeignKey("CityId")]
     public City? City { get; set; }
     
