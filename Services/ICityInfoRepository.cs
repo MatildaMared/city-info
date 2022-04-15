23bc694195c2aa36661c66c5ea7b3b4c09d76be3
Matilda Mared <matildamared@live.se>
2022-04-15 07:54:30 +0200

    add cityinfo repository interface

---
+++ Services/ICityInfoRepository.cs
@@ -0,0 +1,11 @@
+using CityInfo.Entities;
+
+namespace CityInfo.Services;
+
+public interface ICityInfoRepository
+{
+    Task<IEnumerable<City>> GetCitiesAsync();
+    Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest);
+    Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
+    Task<PointOfInterest> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId);
+}
